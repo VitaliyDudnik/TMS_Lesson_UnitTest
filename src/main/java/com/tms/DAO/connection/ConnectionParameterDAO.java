5bb4ef2c635ef538914583cdb0937bdc5157a677
VitaliyDudnik <82210074+VitaliyDudnik@users.noreply.github.com>
2021-08-03 18:02:50 +0300

    Update ConnectionParameterDAO.java

--- src/main/java/com/tms/DAO/connection/ConnectionParameterDAO.java
+++ src/main/java/com/tms/DAO/connection/ConnectionParameterDAO.java
@@ -3,7 +3,7 @@ package com.tms.DAO.connection;
 public class ConnectionParameterDAO {
     private final String url = "jdbc:postgresql://localhost:5432/postgres";
     private final String user = "postgres";
-    private final String password = "jxhpw5GZq6J";
+    private final String password = "*****";
 
     public String getUrl() { return url; }
 
